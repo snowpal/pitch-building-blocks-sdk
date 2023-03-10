4c43eac89fe3064770ee092bd398709e2c9a84fe
AnishGhumara <khatri.anis@gmail.com>
2023-03-11 00:31:18 +0530

    enh: added comments endpoints for block, key pod & block pod

--- lib/golang/endpoints/block_pods.5.comments/block_pods.5.1.get_block_pod_comments.go
+++ lib/golang/endpoints/block_pods.5.comments/block_pods.5.1.get_block_pod_comments.go
@@ -1,36 +1,56 @@
 package block_pods_5
 
 import (
+	"development/go/recipes/lib/golang"
 	"development/go/recipes/lib/golang/helpers"
+	"development/go/recipes/lib/golang/structs/request"
+	"development/go/recipes/lib/golang/structs/response"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 )
 
-func main(jwtToken string) error {
-
-	url := "block-pods/%s/comments?keyId=%s&blockId=%s"
-	method := "GET"
-
+func GetBlockPodComments(jwtToken string, commentParam request.CommentIdParam) ([]response.Comment, error) {
+	resComments := response.Comments{}
 	client := &http.Client{}
-	req, err := http.NewRequest(http.MethodPatch, helpers.GetRoute(golang), nil)
-
+	route, err := helpers.GetRoute(
+		golang.RouteBlockPodsGetBlockPodComments,
+		*commentParam.PodId,
+		commentParam.KeyId,
+		*commentParam.BlockId,
+	)
 	if err != nil {
 		fmt.Println(err)
-		return err
+		return resComments.Comments, err
 	}
+
+	req, err := http.NewRequest(http.MethodGet, route, nil)
+	if err != nil {
+		fmt.Println(err)
+		return resComments.Comments, err
+	}
+
 	helpers.AddUserHeaders(jwtToken, req)
 
 	res, err := client.Do(req)
 	if err != nil {
 		fmt.Println(err)
-		return err
+		return resComments.Comments, err
 	}
+
 	defer helpers.CloseBody(res.Body)
 
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
 		fmt.Println(err)
-		return err
+		return resComments.Comments, err
 	}
+
+	err = json.Unmarshal(body, &resComments)
+	if err != nil {
+		fmt.Println(err)
+		return resComments.Comments, err
+	}
+	return resComments.Comments, nil
 }
--- lib/golang/structs/request/comment.struct.go
+++ lib/golang/structs/request/comment.struct.go
@@ -1,7 +1,13 @@
 package request
 
-type Comment struct {
-	ID            *string   `json:"id"`
-	CommentText   *string   `json:"commentText"`
+type CommentReqBody struct {
+	CommentText   string    `json:"commentText"`
 	TaggedUserIds *[]string `json:"taggedUserIds"`
+}
+
+type CommentIdParam struct {
+	KeyId     string
+	BlockId   *string
+	PodId     *string
+	CommentId *string
 }
