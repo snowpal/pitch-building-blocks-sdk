1fea6d0eb14d83df4a98d3978306bdb17a28bd94
AnishGhumara <khatri.anis@gmail.com>
2023-03-25 02:13:47 +0530

    feat: added endpoints for go status

---
+++ lib/go-status/server.constants.go
@@ -0,0 +1,8 @@
+package go_status
+
+const (
+	XApiKey      = "REPLACE_WITH_YOUR_KEY"
+	XProductCode = "REPLACE_WITH_PRODUCT_CODE"
+)
+
+const GatewayHost = "https://gateway.snowpal.com/"
---
+++ lib/go-status/user.constants.go
@@ -0,0 +1,7 @@
+package go_status
+
+const Password = "Welcome1!"
+
+const (
+	DefaultEmail = "apiuserkp5@gmail.com"
+)
