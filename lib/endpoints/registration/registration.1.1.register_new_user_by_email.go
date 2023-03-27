b4c0c157cdbf40e639f12661258a87b31ecf8719
krish palaniappan <krish@snowpal.software>
2023-03-27 17:08:05 -0400

    Update registration.1.1.register_new_user_by_email.go

--- lib/endpoints/registration/registration.1.1.register_new_user_by_email.go
+++ lib/endpoints/registration/registration.1.1.register_new_user_by_email.go
@@ -8,20 +8,20 @@ import (
 	"strings"
 
 	"github.com/snowpal/pitch-building-blocks-sdk/lib"
-	helpers2 "github.com/snowpal/pitch-building-blocks-sdk/lib/helpers"
+	"github.com/snowpal/pitch-building-blocks-sdk/lib/helpers"
 	"github.com/snowpal/pitch-building-blocks-sdk/lib/structs/request"
 	"github.com/snowpal/pitch-building-blocks-sdk/lib/structs/response"
 )
 
 func RegisterNewUserByEmail(reqBody request.SignupReqBody) (response.User, error) {
 	resUserRegistration := response.UserRegistration{}
-	requestBody, err := helpers2.GetRequestBody(reqBody)
+	requestBody, err := helpers.GetRequestBody(reqBody)
 	if err != nil {
 		fmt.Println(err)
 		return resUserRegistration.User, err
 	}
 	payload := strings.NewReader(requestBody)
-	route, err := helpers2.GetRoute(lib.RouteRegistrationRegisterNewUserByEmail)
+	route, err := helpers.GetRoute(lib.RouteRegistrationRegisterNewUserByEmail)
 	if err != nil {
 		fmt.Println(err)
 		return resUserRegistration.User, err
@@ -33,15 +33,15 @@ func RegisterNewUserByEmail(reqBody request.SignupReqBody) (response.User, error
 		return resUserRegistration.User, err
 	}
 
-	helpers2.AddAppHeaders(req)
+	helpers.AddAppHeaders(req)
 
-	res, err := helpers2.MakeRequest(req)
+	res, err := helpers.MakeRequest(req)
 	if err != nil {
 		fmt.Println(err)
 		return resUserRegistration.User, err
 	}
 
-	defer helpers2.CloseBody(res.Body)
+	defer helpers.CloseBody(res.Body)
 
 	body, _ := io.ReadAll(res.Body)
 	if err != nil {
